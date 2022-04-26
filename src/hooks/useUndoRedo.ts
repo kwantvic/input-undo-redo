6eaae744d1c53b0d3426b66eafa8b2a3c4a0a4f5
kwantvic <kwantvic@gmail.com>
2022-04-26 23:02:05 +0300

    useUndoRedo

--- src/hooks/useUndoRedo.ts
+++ src/hooks/useUndoRedo.ts
@@ -1,10 +1,23 @@
+import React from "react";
+
 interface State {
     index: number;
     text: string[];
 }
 
+enum ActionType {
+    PUSH,
+    UNDO,
+    REDO
+}
+
 type Action = {
-    
+    type: ActionType.PUSH,
+    payload: string
+} | {
+    type: ActionType.UNDO
+} | {
+    type: ActionType.REDO
 }
 
 const initState: State = {
@@ -12,13 +25,34 @@ const initState: State = {
     text: []
 }
 
-const reducer = ({index, state}: State, action: Action): State => {
+const reducer = ({index, text}: State, action: Action): State => {
     switch (action.type) {
+        case ActionType.PUSH:
+            return {
+                index: index + 1,
+                text: [...text.slice(0, index + 1), action.payload]
+            };
+        case ActionType.UNDO:
+            return {
+                index: Math.min(index + 1, text.length),
+                text
+            };
+        case ActionType.REDO:
+            return {
+                index: Math.max(index, 0),
+                text
+            };
         default:
-            return state
+            return {index, text}
     }
 }
 
 export function useUndoRedo() {
-    
+    const [{index, text}, dispatch] = React.useReducer(reducer, initState);
+    return [
+        text[index],
+        (payload: string) => dispatch({type: ActionType.PUSH, payload}),
+        () => dispatch({type: ActionType.UNDO}),
+        () => dispatch({type: ActionType.REDO})
+    ];
 }
