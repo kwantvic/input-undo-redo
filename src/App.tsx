73986aaa7b8eb6c8328d88a3f05c6cc56a892c20
kwantvic <kwantvic@gmail.com>
2022-04-26 22:16:47 +0300

    styles App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export const App: React.FC = React.memo(() => {
-  return (
-      <div className="App">
-        test App
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className="header">
+                <button>undo</button>
+                <button>redo</button>
+                <button>clear</button>
+            </div>
+            <div className="input-block">
+                <input type="text"/>
+                <button>save</button>
+            </div>
+            <ul className="result">
+                <li>test result</li>
+            </ul>
+        </div>
+    );
 })
